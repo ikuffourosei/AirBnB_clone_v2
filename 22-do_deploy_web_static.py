02aa8be546c50f2bb3c96c10701fcad948dc1333
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-03-12 21:17:17 +0000

    uploading archive remotely

--- 22-do_deploy_web_static.py
+++ 22-do_deploy_web_static.py
@@ -4,7 +4,7 @@
 
 
 import os
-from fabric.api import env, put, sudo
+from fabric.api import env, put, run
 from sys import argv
 
 
@@ -19,38 +19,43 @@ def do_deploy(archive_path):
         If the file doesn't exist at archive_path or an error occurs - False.
         if successful - True.
     """
-    if not os.path.isfile(archive_path) is True:
+    if not os.path.isfile(archive_path) is False:
         return False
 
     # generating the remote file name and path
     base_name = os.path.basename(archive_path)  # with .tgz
     file_name = os.path.splitext(base_name)[0]  # without .tgz
-    remote_dir = "/tmp/{}".format(base_name)
 
     # upload archive to /tmp/ directory
-    if put(archive_path, "{}".format(remote_dir)).failed is True:
+    if put(archive_path, "/tmp/{}".format(base_name)).failed is True:
         return False
 
     # uncompress the archive
-    new_dir = '/data/web_static/releases/{}/'.format(file_name)
-    if sudo("rm -rf {}".format(new_dir)).failed is True:
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(file_name)).failed is True:
         return False
-    if sudo("mkdir -p {}".format(new_dir)).failed is True:
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(file_name)).failed is True:
         return False
-    if sudo("tar -xzf {} -C {}".format(remote_dir, new_dir)).failed is True:
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(base_name, file_name)).failed is True:
         return False
 
     # deleting archive from web server
-    if sudo("rm {}".format(remote_dir)).failed is True:
+    if run("rm /tmp/{}".format(base_name)).failed is True:
         return False
-    if sudo("mv {}web_static/* {}".format(new_dir, new_dir)).failed is True:
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(file_name, file_name))\
+            .failed is True:
         return False
-    if sudo("rm -rf {}web_static".format(new_dir)).failed is True:
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(file_name)).failed is True:
         return False
-    if sudo("rm -rf /data/web_static/current").failed is True:
+    if run("rm -rf /data/web_static/current").failed is True:
         return False
 
     # creating a new symbolic link
-    if sudo("ln -s {} /data/web_static/current".format(new_dir)).failed is True:
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(file_name)).failed is True:
         return False
     return True
