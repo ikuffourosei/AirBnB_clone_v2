06e60016c155156c2fd7aa84e0ca7c5f7f39635c
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-03-26 22:23:09 +0000

    AirBnB models updated

--- models/base_model.py
+++ models/base_model.py
@@ -1,70 +1,65 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
-import models
-from uuid import uuid4
+"""This module defines a base class for all models in our hbnb clone"""
+import uuid
 from datetime import datetime
+import sys
+from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column
-from sqlalchemy import DateTime
-from sqlalchemy import String
+import models
 
 Base = declarative_base()
 
 
 class BaseModel:
-    """Defines the BaseModel class.
-
-    Attributes:
-        id (sqlalchemy String): The BaseModel id.
-        created_at (sqlalchemy DateTime): The datetime at creation.
-        updated_at (sqlalchemy DateTime): The datetime of last update.
-    """
-
-    id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    """A base class for all hbnb models"""
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
-
-        Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
-        """
-        self.id = str(uuid4())
-        self.created_at = self.updated_at = datetime.utcnow()
+        """Instatntiates a new model"""
+        self.id = str(uuid.uuid4())
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def save(self):
-        """Update updated_at with the current datetime."""
-        self.updated_at = datetime.utcnow()
+        """Updates updated_at with current time when instance is changed"""
+        self.updated_at = datetime.now()
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self):
-        """Return a dictionary representation of the BaseModel instance.
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
 
-        Includes the key/value pair __class__ representing
-        the class name of the object.
-        """
-        my_dict = self.__dict__.copy()
+    def __repr__(self):
+        """return string representaion"""
+        return self.__str__()
+
+    def to_dict(self):
+        """Convert instance into dict format"""
+        my_dict = dict(self.__dict__)
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
-        my_dict.pop("_sa_instance_state", None)
+        if '_sa_instance_state' in my_dict.keys():
+            del my_dict['_sa_instance_state']
         return my_dict
 
     def delete(self):
-        """Delete the current instance from storage."""
+        """delete the current instance from storage"""
         models.storage.delete(self)
-
-    def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
-        d = self.__dict__.copy()
-        d.pop("_sa_instance_state", None)
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, d)
--- models/review.py
+++ models/review.py
@@ -1,24 +1,12 @@
 #!/usr/bin/python3
-"""Defines the Review class."""
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import ForeignKey
-from sqlalchemy import String
-from sqlalchemy.orm import relationship
+"""This is the review class"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from models.base_model import BaseModel, Base
 
 
 class Review(BaseModel, Base):
-    """Represents a review for a MySQL database.
-
-    Inherits from SQLAlchemy Base and links to the MySQL table reviews.
-
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store Reviews.
-        text (sqlalchemy String): The review description.
-        place_id (sqlalchemy String): The review's place id.
-        user_id (sqlalchemy String): The review's user id.
-    """
+    """This is the class for Review"""
     __tablename__ = "reviews"
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
