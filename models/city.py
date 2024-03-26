195b231405ae74c8e4bd9295d4dd07e8ec02de75
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-03-26 22:53:06 +0000

    AirBnB models updated

--- models/city.py
+++ models/city.py
@@ -14,6 +14,7 @@ class City(BaseModel, Base):
         name: input name
     """
     __tablename__ = "cities"
+    id = Column(String(60), primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     places = relationship("Place", cascade='all, delete, delete-orphan',
