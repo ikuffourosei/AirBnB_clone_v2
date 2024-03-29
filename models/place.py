546e118c1500cc6c862e50c894f47ba3379fb101
ikuffourosei <oseikuffouremmanuel77@gmail.com>
2024-03-29 02:39:36 +0000

    AirBnB models updated

--- models/place.py
+++ models/place.py
@@ -6,7 +6,6 @@ import shlex
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
 
 
 place_amenity = Table("place_amenity", Base.metadata,
@@ -23,7 +22,7 @@ place_amenity = Table("place_amenity", Base.metadata,
 class Place(BaseModel, Base):
     """This is the class for Place"""
     __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    city_id = Column(String(80), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024))
